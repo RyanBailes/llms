d8666d8b5196d2821ce9068303a56ad83316f767
RyanBailes <ryan@baileszindler.com>
2025-08-01 10:50:24 -0500

    Update sheets

--- api/llms.js
+++ api/llms.js
@@ -6,7 +6,7 @@ export default async function handler(req) {
   const ip = req.headers.get("x-forwarded-for") || "unknown";
   const ua = req.headers.get("user-agent") || "unknown";
 
-  await fetch("https://script.google.com/macros/s/AKfycbwzA6g_QhQaQyuCpImAQpFI2PRYe_kwBq6G41YGugCMctrfuxy-DJNQAqACyzte2DHsKg/exec", {
+  await fetch("https://script.google.com/macros/s/AKfycbxx5isepOUcsm7xaTG3fm3JbnP3w0UMYXNd34U2sym8H1hD-gKK5QlCjienWJduwcvlJg/exec", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
